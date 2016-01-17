8707d7c23c3432a2d592397c9783d778b8a4fe7e
Florent Berbie <florent-berbie@hotmail.fr>
2016-01-17 17:04:15 +0100

    Refactor all project

---
+++ MakeAFocal/app/src/main/java/com/m2dl/maf/makeafocal/MainActivity.java
@@ -0,0 +1,194 @@
+package com.m2dl.maf.makeafocal;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView =
+                (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                new LatLng(43.56053780000001, 1.468691900000067)));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17F));
+
+
+        //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_user) {
+            // Handle the camera action
+        } else if (id == R.id.nav_filter) {
+
+        } else if (id == R.id.nav_add_location) {
+
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_about) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void onPhotoButtonClick(View view) {
+        Intent intent = new Intent(
+                MainActivity.this, TakePhotoActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMyLocationButtonClick(View view) {
+        moveToMyLocation();
+    }
+
+    private void moveToMyLocation() {
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE );
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        } else {
+            Location location = mMap.getMyLocation();
+            mMap.moveCamera(
+                    CameraUpdateFactory.newLatLng(
+                            new LatLng(
+                                    location.getLatitude(),
+                                    location.getLongitude())));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(17F));
+        }
+
+    }
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.alert_msg_no_gps)
+                .setCancelable(false)
+                .setPositiveButton(
+                        R.string.yes,
+                        new DialogInterface.OnClickListener() {
+                    public void onClick(
+                        @SuppressWarnings("unused")
+                        final DialogInterface dialog,
+                        @SuppressWarnings("unused") final int id) {
+                        startActivity(
+                            new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton(
+                        R.string.no,
+                        new DialogInterface.OnClickListener() {
+                    public void onClick(
+                        final DialogInterface dialog,
+                        @SuppressWarnings("unused") final int id) {
+                            dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+}
---
+++ MakeAFocal/app/src/main/java/com/m2dl/maf/makeafocal/TakePhotoActivity.java
@@ -0,0 +1,108 @@
+package com.m2dl.maf.makeafocal;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Calendar;
+
+/**
+ * Created by florent on 17/01/16.
+ */
+public class TakePhotoActivity extends Activity {
+    private Uri imageUri;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ImageView image = new ImageView(this);
+        setContentView(image);
+        takePhoto(image);
+    }
+
+    /**
+     * Take a photo in the <i>view</i>.
+     * @param view View to take a photo.
+     */
+    public void takePhoto(View view) {
+        //Création d'un intent
+        createDirIfNotExists();
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+        //Récupération de l'heure pour le nom fichier
+        Calendar c = Calendar.getInstance();
+        int seconds = c.get(Calendar.SECOND);
+
+
+
+        //Création du fichier image
+        File photo = new File(Environment.getExternalStorageDirectory()+"/MakeAFocalPoint",  String.valueOf(seconds)+ ".jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+
+        //On lance l'intent
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    //On a reçu le résultat d'une activité
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            //Si l'activité était une prise de photo
+            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+
+                    //ImageView imageView = (ImageView) findViewById(R.id.ImageView);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+
+                        //imageView.setImageBitmap(bitmap);
+                        //Affichage de l'infobulle
+                        Toast.makeText(
+                                this,
+                                selectedImage.toString(),
+                                Toast.LENGTH_LONG)
+                                .show();
+
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
+        }
+    }
+
+
+    public void  createDirIfNotExists() {
+        File folder = new File(Environment.getExternalStorageDirectory() + "/MakeAFocalPoint");
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdir();
+        }
+        if (success) {
+            Toast.makeText(this,"Repertoire créer", Toast.LENGTH_SHORT);
+        } else {
+            Toast.makeText(this,"Failed to create", Toast.LENGTH_SHORT);
+        }
+    }
+}
