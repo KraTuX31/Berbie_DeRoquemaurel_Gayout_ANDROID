19a7020167295e77ca3b681c7341801fc941d686
Marc Gayout <mgayout@hotmail.fr>
2016-01-17 11:24:24 +0100

    #18 commit de la prise de photo local

--- MakeAFocalpoint/app/src/main/java/com/m2dl/maf/makeafocalpoint/takePhotoActivity.java
+++ MakeAFocalpoint/app/src/main/java/com/m2dl/maf/makeafocalpoint/takePhotoActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.Calendar;
 
 /**
  * Created by Marc on 15/01/2016.
@@ -21,13 +23,28 @@ public class TakePhotoActivity extends Activity {
     private Uri imageUri;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ImageView image = new ImageView(this);
+        setContentView(image);
+        takePhoto(image);
+    }
 
-    public void takePhoto() {
+    //On veut prendre une photo
+    public void takePhoto(View view) {
         //Création d'un intent
+        createDirIfNotExists();
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
 
+        //Récupération de l'heure pour le nom fichier
+        Calendar c = Calendar.getInstance();
+        int seconds = c.get(Calendar.SECOND);
+
+
+
         //Création du fichier image
-        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        File photo = new File(Environment.getExternalStorageDirectory()+"/MakeAFocalPoint",  String.valueOf(seconds)+ ".jpg");
         intent.putExtra(MediaStore.EXTRA_OUTPUT,
                 Uri.fromFile(photo));
         imageUri = Uri.fromFile(photo);
@@ -46,12 +63,15 @@ public class TakePhotoActivity extends Activity {
                 if (resultCode == Activity.RESULT_OK) {
                     Uri selectedImage = imageUri;
                     getContentResolver().notifyChange(selectedImage, null);
+
+                    //ImageView imageView = (ImageView) findViewById(R.id.ImageView);
                     ContentResolver cr = getContentResolver();
                     Bitmap bitmap;
                     try {
                         bitmap = android.provider.MediaStore.Images.Media
                                 .getBitmap(cr, selectedImage);
 
+                        //imageView.setImageBitmap(bitmap);
                         //Affichage de l'infobulle
                         Toast.makeText(this, selectedImage.toString(),
                                 Toast.LENGTH_LONG).show();
@@ -63,6 +83,25 @@ public class TakePhotoActivity extends Activity {
                 }
         }
     }
+
+
+    public void  createDirIfNotExists() {
+        File folder = new File(Environment.getExternalStorageDirectory() + "/MakeAFocalPoint");
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdir();
+        }
+        if (success) {
+            Toast.makeText(this,"Repertoire créer", Toast.LENGTH_SHORT);
+        } else {
+            Toast.makeText(this,"Failed to create", Toast.LENGTH_SHORT);
+        }
+    }
+
+
+
+
+
 
 
 
