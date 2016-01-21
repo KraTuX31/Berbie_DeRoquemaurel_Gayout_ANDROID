1508ab10f04628f13bb0b076b9f27a65bf562df4
Florent Berbie <florent-berbie@hotmail.fr>
2016-01-21 23:42:58 +0100

    Dynamic add of tag

--- MakeAFocal/app/src/main/java/com/m2dl/maf/makeafocal/TakePhotoActivity.java
+++ MakeAFocal/app/src/main/java/com/m2dl/maf/makeafocal/TakePhotoActivity.java
@@ -11,18 +11,35 @@ import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.m2dl.maf.makeafocal.adapter.TagsArrayAdapter;
+import com.m2dl.maf.makeafocal.model.Tag;
+
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by florent on 17/01/16.
  */
 public class TakePhotoActivity extends Activity {
+    /** URI of the image file. */
     private Uri imageUri;
+    /** Container for the photo taken. */
     private ImageView imageView;
+    /** List of tags. */
+    private List<Tag> tags = new ArrayList<>();
+    private TagsArrayAdapter tagsAdapter;
+    /** View containing the list of existing tags. */
+    private ListView listView;
+
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     @Override
@@ -30,6 +47,10 @@ public class TakePhotoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tags_to_image);
         imageView = (ImageView) findViewById(R.id.iv_photo_to_tag);
+        listView = (ListView) findViewById(R.id.list_tags_added);
+
+        tagsAdapter = new TagsArrayAdapter(this, tags);
+        listView.setAdapter(tagsAdapter);
 
         takePhoto();
     }
@@ -46,13 +67,10 @@ public class TakePhotoActivity extends Activity {
         Calendar c = Calendar.getInstance();
         int seconds = c.get(Calendar.SECOND);
 
-
-
         //Création du fichier image
         File photo = new File(
-                Environment.getExternalStorageDirectory()
-                        + "/" + R.string.maf_repository,
-                String.valueOf(seconds)+ ".jpg");
+            Environment.getExternalStorageDirectory() + "/" + R.string.maf_repository,
+            String.valueOf(seconds)+ ".jpg");
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
         imageUri = Uri.fromFile(photo);
 
@@ -65,54 +83,45 @@ public class TakePhotoActivity extends Activity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
-            //Si l'activité était une prise de photo
-            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri selectedImage = imageUri;
-                    getContentResolver().notifyChange(selectedImage, null);
+        //Si l'activité était une prise de photo
+        case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
+            if (resultCode == Activity.RESULT_OK) {
+                Uri selectedImage = imageUri;
+                getContentResolver().notifyChange(selectedImage, null);
 
-                    //ImageView imageView = (ImageView) findViewById(R.id.ImageView);
-                    ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
-                    try {
-                        bitmap = android.provider.MediaStore.Images.Media
-                                .getBitmap(cr, selectedImage);
+                ContentResolver cr = getContentResolver();
+                Bitmap bitmap;
+                try {
+                    bitmap = android.provider.MediaStore.Images.Media
+                            .getBitmap(cr, selectedImage);
 
-                        imageView.setImageBitmap(bitmap);
-                        //Affichage de l'infobulle
-//                        Toast.makeText(
-//                                this,
-//                                selectedImage.toString(),
-//                                Toast.LENGTH_LONG)
-//                                .show();
-
-                    } catch (Exception e) {
-                        Toast.makeText(
-                                this,
-                                R.string.error_take_photo,
-                                Toast.LENGTH_SHORT)
-                                .show();
-                        Log.e("Camera", e.toString());
-                    }
+                    imageView.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    Toast.makeText(
+                            this,
+                            R.string.error_take_photo,
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    Log.e("Camera", e.toString());
                 }
+            }
         }
     }
 
+    public void onAddTagButtonClick(final View view) {
+        TextView tv = (TextView) findViewById(R.id.tv_add_new_tag);
+        tags.add(new Tag(tv.getText().toString()));
+        tagsAdapter.notifyDataSetChanged();
+    }
 
-    public void  createDirIfNotExists() {
+    public boolean  createDirIfNotExists() {
         File folder = new File(
-                Environment.getExternalStorageDirectory()
-                        + "/" + R.string.maf_repository);
+            Environment.getExternalStorageDirectory() + "/" + R.string.maf_repository);
         boolean success = true;
         if (!folder.exists()) {
             success = folder.mkdir();
         }
-        if (success) {
-            Toast.makeText(
-                    this, R.string.repository_created, Toast.LENGTH_SHORT);
-        } else {
-            Toast.makeText(
-                    this, R.string.repository_fail_created, Toast.LENGTH_SHORT);
-        }
+
+        return success;
     }
 }
