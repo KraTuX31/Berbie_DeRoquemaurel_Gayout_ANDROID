d4345c4d32da111ee3c57fd1778c67b055ff6755
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2016-01-21 14:27:37 +0100

    Initialize models (Photo, Tag and Zone)

---
+++ MakeAFocal/app/src/main/java/com/m2dl/maf/makeafocal/model/Photo.java
@@ -0,0 +1,33 @@
+package com.m2dl.maf.makeafocal.model;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by aroquemaurel on 21/01/16.
+ */
+public class Photo {
+    private Bitmap image; // The Bitmap image of the photo
+    private Location location; // The position of the photo
+    private Set<Tag> tags; // Tags of the photo
+
+    /**
+     * Create an empty photo
+     * TODO : search usefull constructors
+     */
+    public Photo() {
+        this.tags = new HashSet<>();
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
+    }
+
+    public Set<Tag> getTags() {
+        return this.tags;
+    }
+
+}
